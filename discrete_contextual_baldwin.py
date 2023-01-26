f844086d874a0aef97c1aaefe6a9d90ac1a655f7
Aldo Pacchiano <aldopacchiano@gmail.com>
2023-01-26 12:26:35 -0500

    lsakdmf

--- discrete_contextual_baldwin.py
+++ discrete_contextual_baldwin.py
@@ -129,10 +129,13 @@ def run_experiment(num_contexts, parameters, evolver_dimensions,
 if __name__ == '__main__':
 	parameters = [.6, .4, .3, .7]
 	
+	evolver_timesteps = int(sys.argv[0])
+	creature_horizon = int(sys.argv[1])
+	evolver_dimension = int(sys.argv[2])	
 
-	evolver_timesteps = 1000
-	creature_horizon = 1000
-	evolver_dimensions = 2
+	# evolver_timesteps = 1000
+	# creature_horizon = 1000
+	# evolver_dimensions = 2
 	num_experiments = 10
 
 	Ts = np.arange(evolver_timesteps)+1
